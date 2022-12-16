46457dffbb331db122e7b4e89a8d7acb2d1e4674
Dakota Garcia <dakotagarcia@Dakotas-Air.localdomain>
2022-12-16 13:09:52 -0700

    initial commit

---
+++ gitInfo.js
@@ -0,0 +1,90 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+
+ let gitDef = "Git is a control system used to track changes in computer files"
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+
+let gitHubDef = "Github is a website that allows developers a place to store their code, work with other developers and show off their work."
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+
+let gitInitDef = "Git init initializes a new git repository or reinitializes an already existing one."
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+let gitCloneDef = "Git clone is essentially the git version of the copy function."
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+
+let gitStatusDef = "Git status is used tpo check the status of the repository and see changes to the files within."
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+
+let gitAddDef = "Git add is essentially the save function, and is used to update changes to the file and prep the content for a commit."
+let gitAddCode = "git add -A"
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+let gitCommitDef = "it is the proccess of recommiting changes made to the file in the repository. the second step required after the git add function."
+
+let gitCommitCode = "git commit -m 'initial commit'"
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+
+let gitPushDef = "git push essentially pushes code from one repository to another. i.e from your computer to github. "
---
+++ sdlc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfReq = "This is the seconed step, in which developers will breakdown what is required to acheive the desired outcome."
+const design = "This is the third step, Developers will begin to write pseudocode or a rough draft based on what they have deemed neccassary in steps one and two."
+const implementation = "This is the fourth step, developers will begin to write functional code using the outline from step three. "
+const testingInt = "This is the fifth step, in which developers will take their code and test it in everyway to find bugs or problems with it."
+const maintain = " This is the the final step, it involves updating and supporting the software after it has been deemed useable and implemented"
